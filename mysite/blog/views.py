05fc53012b0230e605eb3af3bd7aabb8695d7341
Abdelkhalek Bakkari <bakkari.abdelkhalek@hotmail.fr>
2022-12-03 21:46:59 +0000

    correct pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,13 +20,20 @@ def post_list(request):
                  'blog/post/list.html',
                  {'posts': posts})
 
-def post_detail(request,year,month,day, post):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,
                              status=Post.Status.PUBLISHED,
                              slug=post,
                              publish__year=year,
                              publish__month=month,
                              publish__day=day)
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+
     return render(request,
                   'blog/post/detail.html',
-                  {'post': post})
+                  {'post': post,
+                   'comments': comments,
+                   'form': form})
